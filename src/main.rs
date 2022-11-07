efaa44f586ca02326d5191af0eecb71f167edde2
Your Name <you@example.com>
2022-11-07 07:32:38 +0000

    Running code

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use rs_ws281x::{ChannelBuilder, Controller, ControllerBuilder, StripType};
 use std::net::UdpSocket;
+// use std::{thread, time};
 
 struct LightSystem {
     lights: Vec<Color>,
     controller: Controller,
     socket: UdpSocket,
+    offset: usize,
 }
 
 impl LightSystem {
@@ -12,21 +14,28 @@ impl LightSystem {
         let leds = self.controller.leds_mut(0);
 
         for x in 1..self.lights.len() {
-            self.lights[x] = color_wheel(x as u8);
+            self.lights[x] = color_wheel((self.offset + x) as u8);
         }
 
-        for x in 1..leds.len() {
-            leds[x] = self.lights[x].led_color();
+        for (i, x) in leds.iter_mut().enumerate() {
+            *x = self.lights[i].led_color();
         }
+
+        // let a: Vec<[u8; 4]> = self.lights[0..leds.len()].into_iter().map(|x| x.led_color()).collect();
+        // leds.clone_from_slice(a.as_slice());
 
         let udp_message: Vec<u8> = self.lights[800..].into_iter().map(|x| x.led_color()).flatten().collect();
-
         self.socket.send_to(udp_message.as_slice(), "55.55.55.56:41234").expect("couldn't send data");
+
+        // Wait first, since data might still be flowing out of the serial connection
+        self.controller.wait().unwrap();
         self.controller.render().unwrap();
+
+        self.offset += 1;
     }
 }
 
-#[derive(Clone)]
+#[derive(Clone, Copy)]
 struct Color {
     red: u8,
     green: u8,
@@ -44,7 +53,7 @@ impl Color {
             red: red,
             green: green,
             blue: blue,
-            white: 0,
+            white: 255,
         };
     }
 }
@@ -75,22 +84,23 @@ fn main() {
         .channel(
             0, // Channel Index
             ChannelBuilder::new()
-                .pin(10) // GPIO 10 = SPI0 MOSI
-                .count(64) // Number of LEDs
+                .pin(18) // GPIO 10 = SPI0 MOSI
+                .count(800) // Number of LEDs
                 .strip_type(StripType::Ws2812)
-                .brightness(20) // default: 255
+                .brightness(255) // default: 255
                 .build(),
         )
         .build()
         .unwrap();
 
-    let socket = UdpSocket::bind("127.0.0.1:34254").expect("couldn't bind to address");
+    let socket = UdpSocket::bind("0.0.0.0:34254").expect("couldn't bind to address");
     let lights: Vec<Color> = std::iter::repeat(Color::new(0,0,0)).take(1600).collect(); // Vec::new(1600); 
 
     let mut system = LightSystem {
         controller: controller,
         socket: socket,
         lights: lights,
+        offset: 0,
     };
 
     loop {
